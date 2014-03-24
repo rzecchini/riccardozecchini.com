d46d6263a1b6db05d0f3a83f4505643b5d6fa480
Riccardo Zecchini <zecchini.riccardo@gmail.com>
2014-03-24 15:01:27 +0100

    update

--- behave.js
+++ behave.js
@@ -1,5 +1,5 @@
 // make intro-img fixed
-$(".intro-img").css("position", "fixed");
+//$(".intro-img").css("position", "fixed");
 
 //unveil imgs
 $("img").unveil(400);
@@ -9,7 +9,7 @@ $(".video-container").fitVids();
 
 //reveal menu bar on scroll
 var previousScroll = 0, // previous scroll position
-    menuOffset = 104, // height of menu (once scroll passed it, menu is hidden)
+    menuOffset = 136, // height of menu (once scroll passed it, menu is hidden)
     detachPoint = 800, // point of detach (after scroll passed it, menu is fixed)
     hideShowOffset = 60; // scrolling value after which triggers hide/show menu
 
@@ -28,8 +28,8 @@ $("a").click(function () {
 // on scroll hide/show menu, assign active to current prj, sticky intro-img
 $(window).scroll(function () {
     var currentScroll = $(this).scrollTop(), // gets current scroll position
-        scrollDifference = Math.abs(currentScroll - previousScroll); // calculates how fast user is scrolling
-    
+        scrollDifference = Math.abs(currentScroll - previousScroll); // calculates how fast user is scrolling 
+
     // assign active to current prj
     $('.single-project').each(function (i) {
         if ($(this).position().top <= (currentScroll + (menuOffset + 20))) {
@@ -41,56 +41,59 @@ $(window).scroll(function () {
     if (currentScroll <= 600) {
         $('.projects-nav-link.active').removeClass('active');
     }
-    
-    // sticky intro-img
-    if (currentScroll > 400) {
-        $(".intro-img").css("position", "absolute");
-        $(".intro-img").css("top", 400);
-    } else {
-        $(".intro-img").css("position", "fixed");
-        $(".intro-img").css("top", 0);
-    }
-    
-    // if scrolled past menu
-    if (currentScroll > menuOffset) {
-        // if scrolled past detach point add class to fix menu
-        if (currentScroll > detachPoint) {
-            if (!$(".site-header").hasClass("detached")) {
-                $(".site-header").addClass("detached");
-            }
-        }
 
-        // hide/show menu
-        if (currentScroll > previousScroll) {
-            // scrolling down; hide menu
-            if (!$(".site-header").hasClass("invisible")) {
-                $(".site-header").addClass("invisible");
+    if ($root.hasClass("no-touch")) {
+
+        // sticky intro-img
+//        if (currentScroll > 400) {
+//            $(".intro-img").css("position", "absolute");
+//            $(".intro-img").css("top", 150);
+//        } else {
+//            $(".intro-img").css("position", "fixed");
+//            $(".intro-img").css("top", 0);
+//        }
+
+        // if scrolled past menu
+        if (currentScroll > menuOffset) {
+            // if scrolled past detach point add class to fix menu
+            if (currentScroll > detachPoint) {
+                if (!$(".site-header").hasClass("detached")) {
+                    $(".site-header").addClass("detached");
+                }
+            }
+
+            // hide/show menu
+            if (currentScroll > previousScroll) {
+                // scrolling down; hide menu
+                if (!$(".site-header").hasClass("invisible")) {
+                    $(".site-header").addClass("invisible");
+                }
+            } else {
+                // if scrolling faster than hideShowOffset
+                if (scrollDifference >= hideShowOffset) {
+                    // scrolling up; show menu
+                    if ($(".site-header").hasClass("invisible")) {
+                        $(".site-header").removeClass("invisible");
+                    }
+                }
             }
         } else {
-            // if scrolling faster than hideShowOffset
-            if (scrollDifference >= hideShowOffset) {
-                // scrolling up; show menu
+            // only remove “detached” class if user is at the top of document (menu jump fix)
+            if (currentScroll <= 0) {
+                $(".site-header").removeClass("detached");
+
                 if ($(".site-header").hasClass("invisible")) {
                     $(".site-header").removeClass("invisible");
                 }
             }
         }
-    } else {
-        // only remove “detached” class if user is at the top of document (menu jump fix)
-        if (currentScroll <= 0) {
-            $(".site-header").removeClass("detached");
-            
-            if ($(".site-header").hasClass("invisible")) {
-                    $(".site-header").removeClass("invisible");
-                    }
+
+        // if user is at the top of document show menu
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            $(".site-header").removeClass("invisible");
         }
     }
-
-    // if user is at the top of document show menu
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        $(".site-header").removeClass("invisible");
-    }
-
+    
     // replace previous scroll position with new one
     previousScroll = currentScroll;
 });
@@ -102,4 +105,3 @@ $(".project-title").each(function () {
     $(this).prepend('<span class="prj-num">' + prjNum + '</span>');
     prjNum += 1;
 });
-
